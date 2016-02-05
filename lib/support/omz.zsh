cd7755f2d0b3d57f2c8b819678a8aeeae51c4746
b4b4r07 <b4b4r07@gmail.com>
2016-02-05 13:46:17 +0900

    Improve oh-my-zsh load
    Until now, zplug cannot know a library file that the plugin is dependent.
    By introducing __omz_depends function, this is solved.
    It detects in advance the library file to be dependent the plugin
    you want to load, and then load it together.

---
+++ lib/support/omz.zsh
@@ -0,0 +1,49 @@
+__omz_depends() {
+    local    lib_f func_name dep
+    local -a target
+    local -a -U depends
+    local -a func_names
+    local -A omz_libs
+    local omz_repo="$ZPLUG_HOME/repos/$_ZPLUG_OHMYZSH"
+
+    for lib_f in "$omz_repo"/lib/*.zsh(.)
+    do
+        # List funcname in the library file
+        func_names=( ${(@f)"$( \
+            grep "^function" "$lib_f" \
+            | sed 's/^function  *//g' \
+            | sed 's/() {$//g' \
+            | sed 's/ {$//g' \
+            | grep -v " " \
+            )"} )
+
+        # Make list that consists of the funcname and filename
+        for func_name in "${func_names[@]}"
+        do
+            omz_libs[$func_name]="$lib_f"
+        done
+    done
+
+    target=( "$omz_repo/${1:?}"{.zsh-theme,/*.plugin.zsh}(N-.) )
+    for lib_f in "${(k)omz_libs[@]}"
+    do
+        for t in "${target[@]}"
+        do
+            [[ -f $t ]] || continue
+            sed '/^ *#/d' "$t" \
+                | grep "$lib_f" \
+                &>/dev/null &&
+                depends+=( "$omz_libs[$lib_f]" )
+        done
+    done
+
+    # Return dependency list
+    for dep in "${depends[@]}"
+    do
+        echo "$dep"
+    done
+}
+
+__omz_themes() {
+    setopt prompt_subst
+}
