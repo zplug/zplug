zplug/zplug

commit a106eeda6f393eb91ddac33931e3dc0cf97ccdb0

Fix lazy:true from local files/directories
When specifying:

    zplug "$SOME_PATH", from:local, lazy:true

If $SOME_PATH is a path to a file, the parent directory of that file is added
to fpath and the file is autoload-ed. If it's a directory, that directory is
added to fpath but the files in it are not autoload-ed.

base/sources/oh-my-zsh.zsh
@@ -64,7 +64,7 @@ __zplug::sources::oh-my-zsh::load_plugin()
     local    repo="$1"
     local -A tags
     local -a load_fpaths
-    local -a load_plugins
+    local -a unclassified_plugins
     local -a themes_ext
 
     if (( $# < 1 )); then
@@ -77,7 +77,7 @@ __zplug::sources::oh-my-zsh::load_plugin()
     tags=( "${reply[@]}" )
 
     load_fpaths=()
-    load_plugins=()
+    unclassified_plugins=()
     # Themes' extensions for Oh-My-Zsh
     themes_ext=("zsh-theme" "theme-zsh")
 
@@ -85,10 +85,10 @@ __zplug::sources::oh-my-zsh::load_plugin()
     if [[ -z $ZSH ]]; then
         export ZSH="$ZPLUG_REPOS/$_ZPLUG_OHMYZSH"
         export ZSH_CACHE_DIR="$ZSH/cache/"
-        # Insert to the top of load_plugins
-        # load_plugins=(
+        # Insert to the top of unclassified_plugins
+        # unclassified_plugins=(
         #     "$ZSH/oh-my-zsh.sh"
-        #     "${load_plugins[@]}"
+        #     "${unclassified_plugins[@]}"
         # )
         if [[ $tags[name] =~ ^lib ]]; then
             __zplug::utils::omz::theme
@@ -98,20 +98,20 @@ __zplug::sources::oh-my-zsh::load_plugin()
     case $tags[name] in
         plugins/*)
             # TODO: use tag
-            load_plugins=(
+            unclassified_plugins=(
                 ${(@f)"$(__zplug::utils::omz::depends "$tags[name]")"}
                 "$tags[dir]"/*.plugin.zsh(N-.)
             )
             ;;
         themes/*)
             # TODO: use tag
-            load_plugins=(
+            unclassified_plugins=(
                 ${(@f)"$(__zplug::utils::omz::depends "$tags[name]")"}
                 "$tags[dir]".${^themes_ext}(N-.)
             )
             ;;
         lib/*)
-            load_plugins=(
+            unclassified_plugins=(
                 "$tags[dir]"${~tags[use]}
             )
             ;;
@@ -122,7 +122,7 @@ __zplug::sources::oh-my-zsh::load_plugin()
 
     reply=()
     [[ -n $load_fpaths ]] && reply+=( load_fpaths "${(F)load_fpaths}" )
-    [[ -n $load_plugins ]] && reply+=( load_plugins "${(F)load_plugins}" )
+    [[ -n $unclassified_plugins ]] && reply+=( unclassified_plugins "${(F)unclassified_plugins}" )
 
     return 0
 }

autoload/commands/__load__
@@ -12,6 +12,7 @@ local    ignore
 local -A load_commands
 local -a unsorted_repos
 
+local -aU unclassified_plugins
 local -aU load_plugins load_fpaths lazy_plugins nice_plugins
 local -aU ignore_patterns
 
@@ -140,6 +141,9 @@ do
                     "$repo"
                 reply_hash=( "$reply[@]" )
 
+                # Temporary array until files get sorted into
+                # {load,lazy,nice}_plugins
+                unclassified_plugins=( ${(@f)reply_hash[unclassified_plugins]} )
                 # Plugins
                 load_plugins+=( ${(@f)reply_hash[load_plugins]} )
                 lazy_plugins+=( ${(@f)reply_hash[lazy_plugins]} )
@@ -164,6 +168,20 @@ do
             return 1
             ;;
     esac
+
+    # unclassified_plugins -> {nice_plugins,lazy_plugins,load_plugins}
+    if [[ $tags[nice] -gt 9 ]]; then
+        # the order of loading of plugin files
+        nice_plugins+=( "${unclassified_plugins[@]}" )
+    else
+        # autoload plugin / regular plugin
+        if (( $_zplug_boolean_true[(I)$tags[lazy]] )); then
+            lazy_plugins+=( "${unclassified_plugins[@]}" )
+        else
+            load_plugins+=( "${unclassified_plugins[@]}" )
+        fi
+    fi
+    unclassified_plugins=()
 
     if [[ -n $tags[ignore] ]]; then
         ignore_patterns=( $(

base/sources/github.zsh
@@ -99,7 +99,7 @@ __zplug::sources::github::load_plugin()
     local    repo="$1"
     local -A tags default_tags
     local -a plugins_ext themes_ext
-    local -a load_patterns
+    local -a unclassified_plugins
     local    ext default_use
 
     if (( $# < 1 )); then
@@ -115,10 +115,10 @@ __zplug::sources::github::load_plugin()
     # If that is an autoload plugin
     if (( $_zplug_boolean_true[(I)$tags[lazy]] )); then
         if [[ -n $tags[use] ]]; then
-            load_patterns+=( "$tags[dir]"/$tags[use](N.) )
+            unclassified_plugins+=( "$tags[dir]"/$tags[use](N.) )
             load_fpaths+=( "$tags[dir]"/$tags[use]:h(N/) )
         else
-            load_patterns+=( "$tags[dir]/autoload"/*(N.) )
+            unclassified_plugins+=( "$tags[dir]/autoload"/*(N.) )
             load_fpaths+=( "$tags[dir]/autoload"(N/) )
         fi
     else
@@ -137,17 +137,17 @@ __zplug::sources::github::load_plugin()
     do
         if [[ $tags[use] == $default_tags[use] ]]; then
             # NOTE: step 1
-            load_patterns+=( "$tags[dir]"/*.$ext(N-.) )
+            unclassified_plugins+=( "$tags[dir]"/*.$ext(N-.) )
         fi
 
-        if (( $#load_patterns == 0 )); then
+        if (( $#unclassified_plugins == 0 )); then
             # NOTE: step 2
             # If $tags[use] is a regular file,
             # expect to expand to $tags[dir]/*.zsh
-            load_patterns+=( "$tags[dir]"/${~tags[use]}(N.) )
-            if (( $#load_patterns == 0 )); then
+            unclassified_plugins+=( "$tags[dir]"/${~tags[use]}(N.) )
+            if (( $#unclassified_plugins == 0 )); then
                 # For brace
-                load_patterns+=( $(
+                unclassified_plugins+=( $(
                 zsh -c "$_ZPLUG_CONFIG_SUBSHELL; echo $tags[dir]/$tags[use](N.)" \
                     2> >(__zplug::io::log::capture)
                 ) )
@@ -158,11 +158,11 @@ __zplug::sources::github::load_plugin()
             # NOTE: step 3
             # If $tags[use] is a directory,
             # expect to expand to $tags[dir]/*.zsh
-            if (( $#load_patterns == 0 )); then
-                load_patterns+=( $tags[dir]/$tags[use]/$default_tags[use](N.) )
-                if (( $#load_patterns == 0 )); then
+            if (( $#unclassified_plugins == 0 )); then
+                unclassified_plugins+=( $tags[dir]/$tags[use]/$default_tags[use](N.) )
+                if (( $#unclassified_plugins == 0 )); then
                     # For brace
-                    load_patterns+=( $(
+                    unclassified_plugins+=( $(
                     zsh -c "$_ZPLUG_CONFIG_SUBSHELL; echo $tags[dir]/$tags[use]/$default_tags[use](N.)" \
                         2> >(__zplug::io::log::capture)
                     ) )
@@ -174,23 +174,9 @@ __zplug::sources::github::load_plugin()
     done
     fi
 
-    if [[ $tags[nice] -gt 9 ]]; then
-        # the order of loading of plugin files
-        nice_plugins+=( "${load_patterns[@]}" )
-    else
-        # autoload plugin / regular plugin
-        if (( $_zplug_boolean_true[(I)$tags[lazy]] )); then
-            lazy_plugins+=( "${load_patterns[@]}" )
-        else
-            load_plugins+=( "${load_patterns[@]}" )
-        fi
-    fi
-
     reply=()
     [[ -n $load_fpaths ]] && reply+=( load_fpaths "${(F)load_fpaths}" )
-    [[ -n $nice_plugins ]] && reply+=( nice_plugins "${(F)nice_plugins}" )
-    [[ -n $load_plugins ]] && reply+=( load_plugins "${(F)load_plugins}" )
-    [[ -n $lazy_plugins ]] && reply+=( lazy_plugins "${(F)lazy_plugins}" )
+    [[ -n $unclassified_plugins ]] && reply+=( unclassified_plugins "${(F)unclassified_plugins}" )
 }
 
 __zplug::sources::github::load_command()

base/sources/local.zsh
@@ -37,7 +37,7 @@ __zplug::sources::local::load_plugin()
 {
     local    repo="$1"
     local -A tags
-    local -a load_plugins
+    local -a unclassified_plugins
     local -a load_fpaths
     local    expanded_path
     local -a expanded_paths
@@ -59,14 +59,22 @@ __zplug::sources::local::load_plugin()
     for expanded_path in "${expanded_paths[@]}"
     do
         if [[ -f $expanded_path ]]; then
-            load_plugins+=( "$expanded_path" )
+            unclassified_plugins+=( "$expanded_path" )
+
+            # Add parent directory to fpath
+            if (( $_zplug_boolean_true[(I)$tags[lazy]] )); then
+                load_fpaths+=( $expanded_path(N-.:h) )
+            fi
         elif [[ -d $expanded_path ]]; then
-            if [[ -n $tags[use] ]]; then
-                load_plugins+=( $(
+            if (( $_zplug_boolean_true[(I)$tags[lazy]] )); then
+                load_fpaths+=( $expanded_path(N-/) )
+            else
+                # Note: $tags[use] defaults to '*.zsh'
+                unclassified_plugins+=( $(
                 zsh -c "$_ZPLUG_CONFIG_SUBSHELL; echo $expanded_path/$tags[use]" \
                     2> >(__zplug::io::log::capture)
                 ) )
-            else
+
                 load_fpaths+=(
                     "$expanded_path"/{_*,**/_*}(N-.:h)
                 )
@@ -74,7 +82,7 @@ __zplug::sources::local::load_plugin()
         fi
     done
 
-    if (( $#load_plugins == 0 )); then
+    if (( $#unclassified_plugins == 0 )) && (( $#load_fpaths == 0 )); then
         __zplug::io::log::warn \
             "no matching file or directory in $tags[dir]"
         return 1
@@ -82,7 +90,7 @@ __zplug::sources::local::load_plugin()
 
     reply=()
     [[ -n $load_fpaths ]] && reply+=( load_fpaths "${(F)load_fpaths}" )
-    [[ -n $load_plugins ]] && reply+=( load_plugins "${(F)load_plugins}" )
+    [[ -n $unclassified_plugins ]] && reply+=( unclassified_plugins "${(F)unclassified_plugins}" )
 
     return 0
 }

commit 08a862d556c9931679144ce03799a33966c65faa

Fix typo
Variable unclassified_plugins introduced in:
a106eeda6f393eb91ddac33931e3dc0cf97ccdb0

base/sources/oh-my-zsh.zsh
@@ -106,11 +106,11 @@ __zplug::sources::oh-my-zsh::load_plugin()
             )
             # No USE tag specified
             if [[ $tags[use] == '*.zsh' ]]; then
-                load_plugins+=(
+                unclassified_plugins+=(
                     "$tags[dir]/${tags[name]}/"*.plugin.zsh(N-.)
                 )
             else
-                load_plugins+=(
+                unclassified_plugins+=(
                     "$tags[dir]/${tags[name]}/"${~tags[use]}(N-.)
                 )
             fi