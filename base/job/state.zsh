2611f83bf48f17fa86263565ca4b6d01f471799d
b4b4r07 <b4b4r07@gmail.com>
2016-10-25 23:00:52 +0900

    Add __zplug::job::state::flock

--- base/job/state.zsh
+++ base/job/state.zsh
@@ -39,3 +39,26 @@ __zplug::job::state::kill() {
     kill -9 $pid &>/dev/null
     return $status
 }
+
+__zplug::job::state::flock()
+{
+    local file="${1:?}" contents="${2:?}"
+
+    (
+    zsystem flock -t 180 "$file"
+    cant_lock=$status
+    if (( cant_lock > 0 )); then
+        {
+            printf "Can't acquire lock for ${file}."
+            if (( cant_lock == 2 )); then
+                printf " timeout."
+            fi
+            printf "\n"
+        } 1> >(__zplug::io::log::capture)
+        return 1
+    fi
+
+    # Save the status code with LTSV
+    __zplug::io::print::f "$contents\n" >>|"$file"
+    )
+}
