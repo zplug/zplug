164f96003976694a613f28cd6360070be3630897
b4b4r07 <b4b4r07@gmail.com>
2017-01-07 23:51:57 +0900

    Fix #350
    If set globsubst option, this error occur:
    
    ```
    __list__:92: bad pattern: s/(as|use|at|ignore|lazy|commit|dir|of|rename-to|from|do|defer|file|hook-load|depth|if|nice|frozen|hook-build|on):/^[[33m
    ```
    
    and several bug fixes

--- base/core/load.zsh
+++ base/core/load.zsh
@@ -82,8 +82,7 @@ __zplug::core::load::as_plugin()
 
     # Special measure
     if [[ $repo == 'zplug/zplug' ]]; then
-        __zplug::log::write::info \
-            "In the case of zplug, don't load it exceptionally"
+        # In the case of zplug, don't load it exceptionally
         return 0
     fi
 
