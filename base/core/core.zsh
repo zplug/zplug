df26bbc479fcbd2b4878c916cdb2eaad945f866f
b4b4r07 <b4b4r07@gmail.com>
2017-12-29 00:53:17 +0900

    New version 2.4.2

--- base/core/core.zsh
+++ base/core/core.zsh
@@ -176,7 +176,7 @@ __zplug::core::core::variable()
     typeset -gx    ZPLUG_CACHE_DIR=${ZPLUG_CACHE_DIR:-$ZPLUG_HOME/cache}
     typeset -gx    ZPLUG_REPOS=${ZPLUG_REPOS:-$ZPLUG_HOME/repos}
 
-    typeset -gx    _ZPLUG_VERSION="2.4.1"
+    typeset -gx    _ZPLUG_VERSION="2.4.2"
     typeset -gx    _ZPLUG_URL="https://github.com/zplug/zplug"
     typeset -gx    _ZPLUG_OHMYZSH="robbyrussell/oh-my-zsh"
     typeset -gx    _ZPLUG_PREZTO="sorin-ionescu/prezto"
